2877e34b5eb2d9d8507c5939269bbc3718eb70f0
DevFactor <devfactor.github@gmail.com>
2015-11-26 16:06:25 +0000

    Populated pages with posts.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
 
   # back-end code for pages/home
   def home
+    @posts = Post.all
   end
 
   # back-end code for pages/profile
@@ -19,10 +20,12 @@ class PagesController < ApplicationController
       redirect_to root_path, :notice=> "User not found!" 
     end
     
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
   end
 
   # back-end code for pages/explore
   def explore
+    @posts = Post.all
   end
   
 end
