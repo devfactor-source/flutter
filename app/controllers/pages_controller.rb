80bfa820891e8cf8dfa7c42b0445d2573db7cdc9
DevFactor <devfactor.github@gmail.com>
2016-01-13 22:55:26 +0000

    finished styling the profile page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,6 +23,8 @@ class PagesController < ApplicationController
     
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     @newPost = Post.new
+    
+    @toFollow = User.all.last(5)
   end
 
   # back-end code for pages/explore
