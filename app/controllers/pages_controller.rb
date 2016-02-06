077c2e504c33a056c8c6ae9516e6918723fbff6b
DevFactor <devfactor.github@gmail.com>
2016-02-06 19:38:48 +0000

    finished populating pages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,12 @@ class PagesController < ApplicationController
 
   # back-end code for pages/home
   def home
-    @posts = Post.all
+    following = Array.new
+    for @f in current_user.following do
+      following.push(@f.id)
+    end
+
+    @posts = Post.where("user_id IN (?)", following)
     @newPost = Post.new
   end
 
